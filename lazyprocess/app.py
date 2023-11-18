caf2bb40a5f206ea288a7a5e27bd047bb5a047d4
pab-h <dev.pab.2020@gmail.com>
2023-11-18 09:55:19 -0300

    static_folder

--- lazyprocess/app.py
+++ lazyprocess/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from lazyprocess.routes.edit import editRoutes
+from lazyprocess.config import UPLOAD_FOLDER
 
 app = Flask(__name__)
+
+app.static_folder = UPLOAD_FOLDER
 
 app.register_blueprint(editRoutes)
 
